0f2ffc89b9a1d21478f6953b5f7ff843be1ceb40
Paulina <aniskiewicz.paulina@gmail.com>
2019-06-29 19:41:21 +0200

    Logo, Instructions and Link component extracted

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function Logo() {
+  return <img src={logo} className="App-logo" alt="logo" />;
+}
+
+function Instructions() {
+  return <p>React testing</p>;
+}
+
+function LinkedInLink() {
+  return (
+    <a
+      className="LinkedIn"
+      href="https://www.linkedin.com/in/paulina-ani%C5%9Bkiewicz-4b7247179/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      My LinkedIn
+    </a>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Logo />
+        <Instructions />
+        <LinkedInLink />
       </header>
     </div>
   );
