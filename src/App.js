c8d28c24167d46ba93be12c91116d01edd58be5e
Paulina <aniskiewicz.paulina@gmail.com>
2019-07-08 14:57:41 +0200

    Scenes reexported

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-import LandingPage from "./scenes/LandingPage/LandingPage";
-import AboutMe from "./scenes/AboutMe/AboutMe";
-import NoMatch from "./scenes/NoMatch";
+import { LandingPage, AboutMe, NoMatch } from "./scenes";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 function App(props) {
---
+++ src/scenes/index.js
@@ -0,0 +1,5 @@
+import LandingPage from "./LandingPage";
+import AboutMe from "./AboutMe";
+import NoMatch from "./NoMatch";
+
+export { LandingPage, AboutMe, NoMatch };
