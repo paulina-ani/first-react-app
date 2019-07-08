e59bf46bc83f3522ade8251603b3bce389a3deca
Paulina <aniskiewicz.paulina@gmail.com>
2019-07-08 16:15:03 +0200

    Anchor changed to react Link component in NoMatch scene

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./App.css";
 import { LandingPage, AboutMe, NoMatch } from "./scenes";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
 function App(props) {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/landing-page" component={LandingPage} />
         <Route exact path="/aboutme" component={AboutMe} />
+        <Redirect exact from="/" to="landing-page" />
         <Route component={NoMatch} />
       </Switch>
     </Router>
--- src/scenes/NoMatch/NoMatch.js
+++ src/scenes/NoMatch/NoMatch.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { container, bigTitle, link } from "./NoMatch.module.css";
+import { Link } from "react-router-dom";
 
 function NoMatch(params) {
   return (
     <div className={container}>
       <h1 className={bigTitle}>404</h1>
-      <a className={link} href="/">
+      <Link className={link} to="/">
         Go back home
-      </a>
+      </Link>
     </div>
   );
 }
