05f080661443cd1d8206d8ebf6d89275a687b33a
Paulina <aniskiewicz.paulina@gmail.com>
2019-07-10 14:17:41 +0200

    Smaller components extracted in TodoApp

--- src/scenes/TodoApp/TodoApp.js
+++ src/scenes/TodoApp/TodoApp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./TodoApp.module.css";
 import {
   todoapp,
@@ -19,13 +19,101 @@ import {
   selected
 } from "./TodoApp.module.css";
 
-function TodoAppFooter(props) {
+function TodoInput(props) {
+  return (
+    <input className={newTodo} placeholder="What needs to be done?" autofocus />
+  );
+}
+
+function TodoItemCompleted(props) {
+  return (
+    <li className={completed}>
+      <div className={view}>
+        <input className={toggle} type="checkbox" checked />
+        <label className={label}>Taste JavaScript</label>
+        <button className={destroy} />
+      </div>
+      <input className={edit} value="Create a TodoMVC template" />
+    </li>
+  );
+}
+
+function TodoItem(props) {
+  return (
+    <li>
+      <div className={view}>
+        <input className={toggle} type="checkbox" />
+        <label className={label}>Buy a unicorn</label>
+        <button className={destroy} />
+      </div>
+      <input className={edit} value="Rule the web" />
+    </li>
+  );
+}
+
+function ToggleAll(props) {
+  return (
+    <Fragment>
+      <input className={toggleAll} type="checkbox" />
+      <label className={label} for={toggleAll}>
+        Mark all as complete
+      </label>
+    </Fragment>
+  );
+}
+
+function TodoList(props) {
+  return (
+    <ul className={todoList}>
+      <TodoItem />
+      <TodoItemCompleted />
+    </ul>
+  );
+}
+
+function TodoCount(props) {
+  return (
+    <span className={todoCount}>
+      <strong>0</strong> item left
+    </span>
+  );
+}
+
+function TodoFilters(props) {
+  return (
+    <ul className={filters}>
+      <li>
+        <a className={selected} href="#/">
+          All
+        </a>
+      </li>
+      <li>
+        <a href="#/active">Active</a>
+      </li>
+      <li>
+        <a href="#/completed">Completed</a>
+      </li>
+    </ul>
+  );
+}
+
+function TodoFooter(props) {
+  return (
+    <footer className={footer}>
+      <TodoCount />
+      <TodoFilters />
+      <button className="clearCompleted">Clear completed</button>
+    </footer>
+  );
+}
+
+function SiteFooter(props) {
   return (
     <footer className={info}>
       <p>Double-click to edit a todo</p>
       <p>
         Created by
-        <a href="hhttps://github.com/paulina-ani">Paulina Aniśkiewicz</a>
+        <a href="hhttps://github.com/paulina-ani"> Paulina Aniśkiewicz</a>
       </p>
       <p>
         Part of <a href="http://todomvc.com">TodoMVC</a>
@@ -36,59 +124,20 @@ function TodoAppFooter(props) {
 
 function TodoApp(props) {
   return (
-    <section className={todoapp}>
-      <header>
-        <h1>todos</h1>
-        <input
-          className={newTodo}
-          placeholder="What needs to be done?"
-          autofocus
-        />
-      </header>
-      <section className={main}>
-        <input className={toggleAll} type="checkbox" />
-        <label className={label} for={toggleAll}>
-          Mark all as complete
-        </label>
-        <ul className={todoList}>
-          <li className={completed}>
-            <div className={view}>
-              <input className={toggle} type="checkbox" checked />
-              <label className={label}>Taste JavaScript</label>
-              <button className={destroy} />
-            </div>
-            <input className={edit} value="Create a TodoMVC template" />
-          </li>
-          <li>
-            <div className={view}>
-              <input className={toggle} type="checkbox" />
-              <label className={label}>Buy a unicorn</label>
-              <button className={destroy} />
-            </div>
-            <input className={edit} value="Rule the web" />
-          </li>
-        </ul>
+    <div>
+      <section className={todoapp}>
+        <header>
+          <h1>todos</h1>
+          <TodoInput />
+        </header>
+        <section className={main}>
+          <ToggleAll />
+          <TodoList />
+        </section>
+        <TodoFooter />
       </section>
-      <footer className={footer}>
-        <span className={todoCount}>
-          <strong>0</strong> item left
-        </span>
-        <ul className={filters}>
-          <li>
-            <a className={selected} href="#/">
-              All
-            </a>
-          </li>
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-        <button className="clearCompleted">Clear completed</button>
-      </footer>
-    </section>
+      <SiteFooter />
+    </div>
   );
 }
 
